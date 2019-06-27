c7df334ace7db03352824a5b023a93e2de94d788
kor216465 <kor216465@gmail.com>
2019-06-27 17:00:08 +0900

    programmatically segue views

---
+++ app-navigator/Controller/SkillVC.swift
@@ -0,0 +1,31 @@
+//
+//  SkillVC.swift
+//  app-navigator
+//
+//  Created by Jeff Jeong on 2019. 6. 27..
+//  Copyright © 2019년 Tuentuenna. All rights reserved.
+//
+
+import UIKit
+
+// 스킬 뷰 컨트롤러 클래스
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
